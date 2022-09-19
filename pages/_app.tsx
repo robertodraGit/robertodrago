6290b65aae202a29a9320e7f4ab5276b83526067
Roberto Drago <drago@MBP-di-Roberto.homenet.telecomitalia.it>
2022-09-19 09:24:07 +0200

    wip

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,19 @@
 import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import type {AppProps} from 'next/app'
+import {useEffect, useState} from "react";
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+function MyApp({Component, pageProps}: AppProps) {
+    const [color, setColor] = useState();
+
+    useEffect(() => {
+        fetch('http://www.colr.org/json/color/random', {
+            mode: 'no-cors',
+            cache: 'no-cache',
+        })
+            .then(res => console.log(res))
+    }, []);
+
+    return <Component {...pageProps} />
 }
 
 export default MyApp
